296cc7a42ad372d47ee4ef71c369a38212b14c5d
Stefano Pedicino <ss_pedi@outlook.com>
2024-01-17 17:09:37 +0100

    Modified PageState

--- sip-tours-frontend/src/utils/PageState.ts
+++ sip-tours-frontend/src/utils/PageState.ts
@@ -1,23 +1,16 @@
-const BETALENT_PAGE_STATE = "BETALENT_PAGE_STATE";
+const PAGE_STATE = "PAGE_STATE";
 
 export function getPageState<T>(page: string) {
-  const item = localStorage.getItem(BETALENT_PAGE_STATE);
+  const item = localStorage.getItem(PAGE_STATE);
 
-  if (process.env.NODE_ENV === "development") {
-    if (item && item !== "undefined") {
+  if (item) {
       const obj = JSON.parse(item) as T;
       return obj;
     }
-  } else {
-    if (item) {
-      const obj = JSON.parse(item) as T;
-      return obj;
-    }
-  }
 
   throw new Error("The state is undefined");
 }
 
 export function setPageState(page: string, obj: any) {
-  localStorage.setItem(BETALENT_PAGE_STATE, JSON.stringify(obj));
+  localStorage.setItem(PAGE_STATE, JSON.stringify(obj));
 }
