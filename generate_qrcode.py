6b6547460c6383720267b13b25a50a2a81ff6186
Stefano Pedicino <83118026+stefanopedicinogit@users.noreply.github.com>
2024-04-26 16:56:20 +0200

    script to generate site qr code

---
+++ generate_qrcode.py
@@ -0,0 +1,15 @@
+import qrcode
+
+data = "http://www.sip-tour.com"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qrcode.png")
